70f4c5b1bf43bb98c145ccc150b42ca7214a14e4
ow-tech <wagwanwekon@outlook.com>
2024-11-08 03:22:51 +0400

    finished styling navbar, created why schoose us sec tion with carousel, ready for our services

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// nav item on click
+
+// document.querySelectorAll('.dropdown-item').forEach(item => { item.addEventListener('click', event => { // Remove custom-click class from all items 
+//     document.querySelectorAll('.dropdown-item').forEach(el => el.classList.remove('greenBackground')); 
+//     console.log("clikced")
+//     // Add custom-click class to the clicked item 
+//     item.classList.add('greenBackground'); 
+// }); 
+// });
