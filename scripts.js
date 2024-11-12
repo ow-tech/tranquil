4dd2558b7da2bd86e226c0424132f0684790ae56
ow-tech <wagwanwekon@outlook.com>
2024-11-12 10:55:19 +0400

    added emnail intergartion

--- scripts.js
+++ scripts.js
@@ -29,15 +29,6 @@ if (exampleModal) {
   })
 }
 
-// JavaScript for booking confirmation with basic CAPTCHA
-function confirmBooking() {
-  const captchaAnswer = document.getElementById('captchaAnswer').value;
-  if (parseInt(captchaAnswer) === 20) { // example captcha
-      alert('Booking Confirmed!');
-  } else {
-      alert('Incorrect CAPTCHA. Please try again.');
-  }
-}
 
 // Restrict travel date to upcoming dates only
 document.addEventListener('DOMContentLoaded', () => {
@@ -45,3 +36,60 @@ document.addEventListener('DOMContentLoaded', () => {
   const today = new Date().toISOString().split('T')[0];
   travelDateInput.setAttribute('min', today);
 });
+
+// const form = {
+//     from_name: 'Sender',
+//     message: 'Test Message',
+//     reply_to:'alexbarasa0723@gmail.com',
+
+//   };
+const form = document.getElementById('bookingForm')
+function validateForm(e) {
+    console.log('e :', e)
+    e.preventDefault()
+    // Validate Captcha
+    const captchaAnswer = document.getElementById("captchaAnswer").value;
+    if (parseInt(captchaAnswer) !== 20) {
+      alert("Incorrect captcha answer. Please try again.");
+      return false;
+    }
+  
+    // Check Travel Date
+    const travelDate = new Date(document.getElementById("travelDate").value);
+    const today = new Date();
+    if (travelDate < today) {
+      alert("Travel date cannot be in the past.");
+      return false;
+    }
+
+    // Email.send({
+    //     Host: "smtp.elasticemail.com",
+    //     Username: "tranquiltourstravel@gmail.com",
+    //     Password: "5138F83E0C5BDD01204C3701A1F76C11460D",
+    //     To: 'tranquiltourstravel@gmail.com',
+    //     From: "tranquiltourstravel@gmail.com",
+    //     Subject: "Sending Email using javascript",
+    //     Body: "Well that was easy!!",
+    // })
+    //     .then(function (message) {
+    //         alert("mail sent successfully")
+    //     });
+
+        // btn.value = 'Sending...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_69kbfvu';
+     
+        emailjs.sendForm(serviceID, templateID, form)
+         .then(() => {
+        //    btn.value = 'Send Email';
+           alert('Sent!');
+         }, (err) => {
+        //    btn.value = 'Send Email';
+           alert(JSON.stringify(err));
+         });
+    
+  
+    // alert("Booking confirmed successfully!");
+    // return true;
+  }
